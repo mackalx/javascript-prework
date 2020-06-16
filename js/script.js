50c06628aa88fc295a20a16e2660d85907b94445
Maciej Kalita <maciej.kalita@gmail.com>
2020-06-16 13:04:16 +0200

    Add game mechanics with possible  outcomes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-if(randomNumber == 1) {
+if (randomNumber == 1) {
   computerMove = 'kamień';
-} else if(randomNumber == 2) {
+} else if (randomNumber == 2) {
   computerMove = 'papier';
-} else if(randomNumber == 3) {
+} else if (randomNumber == 3) {
   computerMove = 'nożyce';
 }
 
@@ -16,14 +16,32 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'Wybrałeś liczbę spoza zakresu 1 - 3';
+let playerMove = 'Wybrałeś liczbę spoza zakresu.';
 
-if(playerInput == '1') {
+if (playerInput == '1') {
   playerMove = 'kamień';
-} else if(playerInput == '2') {
+} else if (playerInput == '2') {
   playerMove = 'papier';
-} else if(playerInput == '3') {
+} else if (playerInput == '3') {
   playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == playerMove) {
+  printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+  printMessage('Ja wygrywam!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('Ja wygrywam!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Ja wygrywam!');
+} else {
+  printMessage('Spróbuj ponownie.');
+}
